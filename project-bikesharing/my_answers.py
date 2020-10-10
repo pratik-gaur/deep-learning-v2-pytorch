0c1c22a81f6d083713ac2bad162f2301d73abd19
pratik.gaur <pratik.gaur@olx.com>
2020-10-10 20:49:18 +0200

    project bikesharing solution

--- project-bikesharing/my_answers.py
+++ project-bikesharing/my_answers.py
@@ -118,11 +118,7 @@ class NeuralNetwork(object):
 
         '''
         self.weights_hidden_to_output += delta_weights_h_o * self.lr  / n_records # update hidden-to-output weights with gradient descent step
-        print("weights_hidden_to_output is {}".format(self.weights_hidden_to_output))
-        
         self.weights_input_to_hidden +=  delta_weights_i_h * self.lr  / n_records # update input-to-hidden weights with gradient descent step
-        print("weights_input_to_hidden is {}".format(self.weights_input_to_hidden))
-        
 
     def run(self, features):
         ''' Run a forward pass through the network with input features 
@@ -147,7 +143,7 @@ class NeuralNetwork(object):
 #########################################################
 # Set your hyperparameters here
 ##########################################################
-iterations = 1000
+iterations = 2000
 learning_rate = 1
 hidden_nodes = 10
 output_nodes = 1
